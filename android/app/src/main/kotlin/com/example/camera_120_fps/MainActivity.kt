15ed33a74773abe8aef76a9d416f84775e9fcc65
Kam16yu <1vsmsh1@gmail.com>
2023-02-13 11:47:44 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/camera_120_fps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.camera_120_fps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:camera_120_fps/pages/use_camera.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, required });
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter camera demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CameraPage(),
+    );
+  }
+}
